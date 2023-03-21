7e3f452e1d92c7676741ea7af70900148bb43daa
Vincent Lopez <115746291+vlopez99@users.noreply.github.com>
2023-03-20 20:50:49 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Text Editor Code
+
+This is my text editor code follwoing the starter code provided.
+The task, was to create a text editor that can open in a browser window. It will be a single page application and feature a number of data persistence techniques that serve as redundancy in case one of the options is not supported by the browser.
+
+# The application was deployed in Heroku to test
---
+++ client/src/js/database.js
@@ -0,0 +1,33 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+export const putDb = async (content) => {
+  const jateDB = await openDB ('jate', 1);
+  const tx = jateDB.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: 1, value: content });
+  const result = await request;
+  return result.value;
+};
+
+export const getDb = async () => {
+  const jateDB = await openDB('jate', 1);
+  const tx = jateDB.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  return result.value;
+};
+
+initdb();
